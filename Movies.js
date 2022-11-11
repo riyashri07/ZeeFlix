5d78c8b5f5937d2ddde1cacdaa68febcdfc095e2
riyashri07 <riyampct7@gmail.com>
2022-11-11 22:48:11 +0530

    day-4 task done

---
+++ Movies.js
@@ -0,0 +1,31 @@
+let prev = document.getElementById('previous');
+let next = document.getElementById('next');
+
+let arr = ["images/img1.jpg", "images/img2.jpg", "images/img3.jpg","images/img4.jpg","images/img5.jpg","images/img6.jpg"];
+
+let i = 0;
+
+next.addEventListener('click', function () {
+    i++;
+    if (i > arr.length - 1) {
+        i = 0;
+    }
+    document.getElementById('image2').src = arr[i];
+})
+
+//prev btn
+prev.addEventListener('click', function () {
+    i--;
+    if (i < 0) {
+        i = arr.length - 1;
+    }
+    document.getElementById('image2').src = arr[i];
+})
+
+function slides() {
+    document.getElementById("image2").src = arr[i];
+
+    if (i < arr.length - 1) i++;
+    else i = 0;
+  }
+  setInterval(slides, 3000);
